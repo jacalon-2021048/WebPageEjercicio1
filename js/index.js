e5190cf90a3dc02e2535bed8fc08d3e75f15accc
Jhonatan Acalon <jacalon-2021048@kinal.edu.gt>
2023-01-20 08:59:50 -0600

    primer commit v1 webpage

---
+++ js/index.js
@@ -0,0 +1,10 @@
+
+console.log('Hola mundo');
+
+function saludar(){
+    let numA = document.getElementById('num1').value;
+    let numB = document.getElementById('num2').value;
+    let resultado = parseInt(numA) + parseInt(numB);
+    console.log('Click boton');
+    console.log(resultado);
+}
